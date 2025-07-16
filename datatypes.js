2078d028eb7b15e4136f6d0d7d7d7f86ed2cb349
Anushka-deokar <deokaranushka@gmail.com>
2025-07-16 10:27:13 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+
+// Primitive data types
+
+let str = "Anushka Deokar";
+let num = 25;
+let isActive = true;
+let a;
+let value = null;
+let big = 123456789123456789n;
+
+console.log(typeof (str));
+console.log(typeof (num));
+console.log(typeof (isActive));
+console.log(typeof (a));
+console.log(typeof (value));
+console.log(typeof (big));
+
+
+//Non-primitive data types : Arrays , objects, functions 
+
+let colors = ["red", "blue", "orange"];
+console.log(typeof (colors));
+
+let user = { name: " Anushka Deokar" };
+console.log(typeof (user));
+
+
+function greet() {
+    return "Hii Anushka ";
+
+}
+
+console.log(typeof (greet));
+
