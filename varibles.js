531ce787004aee689e94cfc7c5d6c0d4a2c242d2
Anushka-deokar <deokaranushka@gmail.com>
2025-07-16 10:16:02 +0530

    variables

---
+++ varibles.js
@@ -0,0 +1,18 @@
+// var , let and const
+
+var name = "Anushka";
+
+const age = 24;
+//let: can be updated, but not redeclared.
+// const: cannot be updated or redeclared
+
+let score = 10;
+score = 20;
+console.log(score);    // output is 20 
+
+const city = "Pune";
+// city = "Mumbai";  // will throw error
+
+// Use let and const always in modern JavaScript
+
+
