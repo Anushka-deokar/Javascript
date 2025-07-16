a03a39ad0520e02d90bb9a509701adf492e5645b
Anushka-deokar <deokaranushka@gmail.com>
2025-07-16 17:00:07 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,25 @@
+//Arithmetic operators :  +, -, /, *, %, **, 
+let x = 5 + 3;
+let y = 5
+
+5 == "5"     // true
+5 === "5"    // false
+
+true && false // false
+true || false // true
+!true         // false
+
+
+let age = 20;
+let result = age >= 18 ? "Adult" : "Minor"; // "Adult"
+
+//Check if a number is even or odd
+let num = 9;
+let result2 = num % 2 === 0 ? "Even" : "Odd";
+console.log(result2); // "Odd"
+
+
+
+let a = 10;
+let b = 3;
+console.log(a + b, a - b, a * b, a / b, a % b, a ** b);
