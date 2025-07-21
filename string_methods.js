dee400c23ea811b10ec6429a8367fb9f34713d78
Anushka-deokar <deokaranushka@gmail.com>
2025-07-21 13:37:11 +0530

    string methods

---
+++ string_methods.js
@@ -0,0 +1,18 @@
+//String methods
+
+// Methods : actions that can be performed on objects
+// Format : string_name.method()
+
+let msg = " Hello ";
+msg.trim();
+console.log(msg);
+//trims whitespaces from both ends of string and returns a new string
+
+// strings are immutable in JS
+// whenver we try to use any method it returns new string
+
+let name = "Anushka";
+
+console.log(name.toLowerCase());
+
+console.log(name.toUpperCase());
---
+++ string_methods1.js
@@ -0,0 +1,10 @@
+//String Methods with Arguments
+//Argument is some value that we pass to method
+
+//indexOf : returns the first index of occurence of some value in string or gives -1 if not found
+let str = "Anushka Deokar";
+console.log(str.indexOf("Deo"));
+
+console.log(str.indexOf("A"));
+
+
